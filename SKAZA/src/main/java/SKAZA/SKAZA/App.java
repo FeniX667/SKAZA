78570421b831404c0499c6db069d8cd128cc54af
FeniX667 <dmalarczyk@gmail.com>
2014-10-07 14:41:22 +0200

    Start SKAZA

---
+++ SKAZA/src/main/java/SKAZA/SKAZA/App.java
@@ -0,0 +1,13 @@
+package SKAZA.SKAZA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
